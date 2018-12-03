1eeb65a268f0050dbec6a211c9cd62526392b5c0
Alex Wurts <ajwurts@gmail.com>
2018-12-03 11:16:15 -0500

    Graphing Update and Image Processor update

--- graphing.py
+++ graphing.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def graphThings(classes):
+def graphThings(classes, ratio=True):
     count = []
 
     for c in classes:
         counts = c['class'].value_counts()
         total = np.sum(counts)
+        if not ratio:
+            total = 1
         if 'cancer' in counts.index:
             count.append([counts['healthy'] / total, counts['cancer'] / total])
         else:
@@ -18,12 +20,12 @@ def graphThings(classes):
     index = np.arange(len(classes))
     bar_width = 0.35
     _, ax = plt.subplots()
-    ax.bar(index, list(map(lambda x: x[0], count)), bar_width, color='g')
-    ax.bar(index + bar_width, list(map(lambda x: x[1], count)), bar_width, color='r')
-
+    ax.bar(index, list(map(lambda x: x[0], count)), bar_width, color='r', label='Healthy')
+    ax.bar(index + bar_width, list(map(lambda x: x[1], count)), bar_width, color='g', label='Damaged')
+    ax.set_title("Healthy count vs Damaged count by cluster")
+    plt.xticks(())
+    plt.xlabel("Cluster Number")
+    plt.ylabel("Count")
+    ax.legend()
     plt.show()
     
-
-
-
-# if __name__ == "__main__":
